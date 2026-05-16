9c565f0dbad92b21eee37f0beffd8eacef3bae1a
Yigit Duman <yigitq@gmail.com>
2026-05-16 22:59:53 +0100

    fix build errors

--- api-server/src/routes/docs.ts
+++ api-server/src/routes/docs.ts
@@ -7,7 +7,8 @@ const docs = new Hono();
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const html = readFileSync(resolve(__dirname, "../content/docs.html"), "utf-8");
-const favicon = readFileSync(resolve(__dirname, "../content/favicon.png"));
+const faviconBuffer = readFileSync(resolve(__dirname, "../content/favicon.png"));
+const favicon = new Uint8Array(faviconBuffer);
 
 docs.get("/", (c) => {
     return c.html(html);
@@ -16,13 +17,13 @@ docs.get("/", (c) => {
 docs.get("/favicon.png", (c) => {
     c.header("Content-Type", "image/png");
     c.header("Cache-Control", "public, max-age=86400");
-    return c.body(favicon.buffer.slice(favicon.byteOffset, favicon.byteOffset + favicon.byteLength) as ArrayBuffer);
+    return c.body(favicon);
 });
 
 docs.get("/favicon.ico", (c) => {
     c.header("Content-Type", "image/png");
     c.header("Cache-Control", "public, max-age=86400");
-    return c.body(favicon.buffer.slice(favicon.byteOffset, favicon.byteOffset + favicon.byteLength) as ArrayBuffer);
+    return c.body(favicon);
 });
 
 export { docs };
